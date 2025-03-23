1487a693c017284328b888fe3e16c060c38bf5dc
Alireza <akbarkhanakbarzadeh@gmail.com>
2025-03-23 21:23:48 +0330

    fix some bugs

--- app.py
+++ app.py
@@ -19,25 +19,23 @@ def translate_text(text, timeout=10):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--headless")  # بدون رابط کاربری
         chrome_options.add_argument("--disable-gpu")
-
-        # ایجاد درایور با استفاده از فایل درایور محلی
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")        # ایجاد درایور با استفاده از فایل درایور محلی
         service = Service(CHROMEDRIVER_PATH)
         driver = webdriver.Chrome(service=service, options=chrome_options)
         driver.get("https://translate.google.com/?sl=en&tl=fa")
         time.sleep(2)  # زمان برای لود اولیه
     except:
+        driver.quit()
         print("Cant Open Chrome Driver")
         return 0
-
     try:
         input_selector = "textarea.er8xn"
         result_selector = "div.KkbLmb"
         input_element = driver.find_element(By.CSS_SELECTOR, input_selector)
     except:
         driver.quit()
-        print("cant Find Element")
         return 0
-    
     try:
         input_element.clear()
         input_element.send_keys(text)
@@ -45,7 +43,6 @@ def translate_text(text, timeout=10):
         driver.quit()
         print("Cant Enter Inputs")
         return 0
-
     try:
         start_time = time.time()
         while time.time() - start_time < timeout:
@@ -74,7 +71,6 @@ def translate():
     data = request.json
     if "text" not in data:
         return jsonify({"error": "Missing 'text' in request"}), 400
-
     translated_text = translate_text(data["text"])
     if translated_text != 0 or translated_text != None:
         return jsonify({"translated_text": translated_text})
